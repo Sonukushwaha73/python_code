a060d3f2f03b1b2f6dac6c12657793d7e9586185
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-12 11:26:08 +0530

    Add files via upload

---
+++ loopstatement.py
@@ -0,0 +1,83 @@
+
+#range(start stop step) i  variable  in ka mtlb andar jana chahta hu 
+# a=range(1, 50, 2)
+# for i in a:
+#     print(i)
+
+
+
+# step 5
+# a=range(1, 50, 5)
+# for i in a:
+#     print(i)
+
+
+# a=range(50)
+# for i in a:
+#     print(i)
+
+
+
+   #Q1 20-50
+#Q2 16-1
+#Q3 -3  -15
+
+
+# a = range(20,51, 1)
+# for i in a:
+#     print(i)
+
+# a = range(16,1,-1)
+# for i in a:
+#     print(i)
+   
+
+# a = range(3, 15)
+# for i in a:
+#     print(i)
+
+
+
+
+# a = range(-3,-16 ,-1)
+# for i in a:
+#     print(i)
+
+
+
+# #5 table
+# a=range(5,51,5)
+# for i in a:
+#     print(i)
+  
+
+#   #6 table 
+# a=range(6,61,6)
+# for i in a:
+#     print(i)
+
+
+
+          #table
+# n = int(input("enter the number  for table"))
+# for i in range(2,50):
+#     print(n,"*", i, "=", n*i)
+
+
+
+                 #range indexing#
+a="sonukushwaha"
+for i in range(5,8):
+     print(a[i])
+print(len(a)) #lenth nikal sakte hai
+
+
+
+
+
+
+                       #range directly
+
+a="sheriyans is cool "
+for i in a:
+   print(i)                      
