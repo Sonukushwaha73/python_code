afc5be877d8761dabd2d250ec657ea7b702543ae
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-12 11:20:38 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,37 @@
+#assignment operator {+= -= *=+/= //= %= **=}
+a=20
+# a+= 20#yaha value add hota hai
+# print(a)
+
+#output:40
+
+
+
+#subtraction operator
+# a-=25
+# print(a)
+
+
+#output:-5
+
+
+# multiple assingment operator
+
+# a*=4
+# print(a)
+
+#output:80
+
+
+
+# division assginment operator
+# #output 4.0
+
+
+
+#flowdivision operators 
+
+# a//=5
+# print(a)
+
+
