41dc063ecbb8076efca6626eed5d215a064b716f
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-12 11:24:17 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+  
+# name="world"
+# age=67
+# print("hello my name is ",name,"and my age is",age)
+
+#formating string  
+
+ 
+# name="world"
+# age=67
+# print(f"hello my name is {name} and my age is {age}")
+
+
+
+#input #name =input("enter you name")
+age=67
+input("age")
