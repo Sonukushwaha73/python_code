7c443fc4a98b64e3b5fb6b48c5df4e02a623afb5
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-12 11:21:50 +0530

    Add files via upload

---
+++ conditionstatement.py
@@ -0,0 +1,40 @@
+#if excuted if the condition is true
+#if else -execute if true another if false 
+#if-elif-else ec=xceute multiple condition in seequence
+
+
+
+# a=13
+# if a>14:
+#   print("i will do task")
+# else:
+#   print("i will not do task")
+
+
+
+
+   #multiple condition
+# money=int(input("please provid me the money:-"))
+# if money==10:
+#    print("i have a chaocko bar")
+# else:
+#   print("i have the mango")   
+
+
+
+
+
+money = int(input("Enter a number:-"))
+
+if money == 45:
+    print("We are providing the mango")
+
+elif money == 56:
+    print("We are not providing the money")
+elif money == 78:
+    print("We are not providing the eating")
+else:
+    print("We are not providing the food")
+
+
+
