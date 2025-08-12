6ad4aff4c740cb0251968718c43025104fb7f75a
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-12 11:25:32 +0530

    Add files via upload

---
+++ logicopertors.py
@@ -0,0 +1,25 @@
+ # logic operators [&and or// not!]
+
+
+# a="A"
+# b="B"
+# print(ord(a))
+# print(ord(b))
+# print(a>b)
+print(124 > 120 and 34==34 and 45<50)# and bolta hai sab true ho  [or bolta koi ek true kar do]
+
+print(124 < 120 or 34==34 or 45>50)
+
+#! revers value true ko fals aur fals ko true
+print( not 12==12)
+
+
+
+#Q1 answer true and false
+a=12
+b=34
+print(a<b)
+print(a>b)
+
+
+
