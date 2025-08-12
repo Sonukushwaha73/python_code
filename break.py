ca708b1e1f51449031db1b0fac91a820bd85652a
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-12 11:21:04 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,27 @@
+for i in range(1,11):
+    print(i)
+
+
+
+
+#break statement
+    for i in range(1,20):
+        if i ==10:
+            break
+        else:
+            print(i)
+
+
+
+
+
+
+            #1-50
+            for i in range(1,51):
+                if i==45:
+                    print("break statement is ececuted")
+                    break 
+                
+                print(i)
+            else:
+                    print("break statement is not excuted")
