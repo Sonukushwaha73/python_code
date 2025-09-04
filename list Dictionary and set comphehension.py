c7ec00431b5f09d3f5a85497cb690a4f600e831b
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-04 08:57:03 +0530

    list Dictionary and set comphehension.py

---
+++ list Dictionary and set comphehension.py
@@ -0,0 +1,18 @@
+#tanary opertion
+a =10
+print("even") if a%2==0 else print("odd")
+
+
+
+
+#lamda function
+#lamda is a sort function is one line line code
+
+addition = lambda a: "even" if a%2 ==2 else "odd"
+print(addition(2))
+
+
+
+
+addition = lambda a,b : a+b
+print(addition(2,3))
